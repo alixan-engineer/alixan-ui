c5f33bcfb03d4e5dc8421275a6de5f418cc9c0ff
Alikhan <xalidevxx@gmail.com>
2026-06-10 03:07:20 +0500

    fix: update layout class in app.vue for improved styling

--- app/app.vue
+++ app/app.vue
@@ -12,7 +12,9 @@ useHead({
 </script>
 
 <template>
-	<div class="size-full max-w-container mx-auto px-4 py-6 space-y-10 z-40">
+	<div
+		class="size-full max-w-container mx-auto px-4 py-6 flex flex-col gap-10 z-40"
+	>
 		<Header />
 		<Main />
 	</div>
