a090f59121f4cff8175a72acd945131a0b31074f
Alikhan <xalidevxx@gmail.com>
2026-06-10 21:49:24 +0500

    fix: update layout classes in app.vue and tailwind.css for improved styling

--- app/app.vue
+++ app/app.vue
@@ -13,7 +13,7 @@ useHead({
 
 <template>
 	<div
-		class="size-full max-w-container mx-auto px-4 py-6 flex flex-col gap-10 z-40"
+		class="size-full max-w-container mx-auto px-4 py-6 space-y-10 overflow-y-auto z-40"
 	>
 		<Header />
 		<Main />
