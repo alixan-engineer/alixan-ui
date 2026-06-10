b614b346f750a6f00e945bf14fcb93763a482051
Alikhan <xalidevxx@gmail.com>
2026-06-10 19:13:44 +0500

    feat: add back navigation button and update localization files for multiple languages

--- app/components/layout/header/Header.vue
+++ app/components/layout/header/Header.vue
@@ -1,8 +1,22 @@
+<script setup lang="ts">
+import { ArrowLeft } from '@lucide/vue';
+</script>
+
 <template>
 	<header
-		class="w-full h-14 px-6 flex items-center justify-between bg-secondary dark:bg-background/30 border z-30 rounded-full"
+		class="w-full h-14 px-6 flex items-center gap-4 bg-secondary dark:bg-background/30 border z-30 rounded-full"
 	>
+		<IconButton
+			class="max-lg:hidden!"
+			variant="ghost"
+			color="default"
+			href="https://alixan.kz/"
+			:label="$tn('app.back')"
+		>
+			<ArrowLeft />
+		</IconButton>
 		<Logo />
+		<div class="flex-1" />
 		<HeaderSettings />
 	</header>
 </template>
